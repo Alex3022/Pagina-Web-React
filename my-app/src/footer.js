a99e81b7ac234c5eddb1eed577c5985736415f65
Andres Felipe Restrepo Rios <140030376+Andres-F-Restrepo@users.noreply.github.com>
2023-08-27 09:52:21 -0500

    Footer mas botón app

---
+++ my-app/src/footer.js
@@ -0,0 +1,12 @@
+import './footer.css';
+
+function Footer() {
+  return (
+    <div className="image-container">
+        <img src={logopagina} className="logo"/>
+    </div>
+
+  );
+}
+
+export default Footer;
